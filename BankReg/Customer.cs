c72391888d4d2ae7804bf3fdc34710a29a02de5f
Артур <arturskomarohov@gmail.com>
2021-07-29 21:36:35 +0300

    Добавьте файлы проекта.

---
+++ BankReg/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankReg
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Acc { get; set; }
+
+
+
+
+      /*  public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Customer))
+            {
+                return false;
+            }
+            Customer result = (Customer)obj;
+            return Customer. == SideCount && result.SideLength == SideLength;
+        }
+
+        public static bool operator ==(Figure first, Figure second)
+        {
+            return first.Equals(second);
+        }
+        public static bool operator !=(Figure first, Figure second)
+        {
+            return !first.Equals(second);
+        }*/
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ BankReg/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankReg
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, Name = "John Sommers", Acc = 12345 };
+            Customer customer2 = new Customer { Id = 2, Name = "Ivan Bubkin", Acc = 43535 };
+            Customer customer3 = new Customer { Id = 3, Name = "Artur Skomarohov", Acc = 54546 };
+            Customer customer4 = new Customer { Id = 4, Name = "Fedor Chukrov", Acc = 87855 };
+            Customer customer5 = new Customer { Id = 5, Name = "Alla Pugacheva", Acc = 58687 };
+
+
+            List<Customer> customers = new List<Customer>();
+            customers.AddRange(new Customer[] { customer1, customer2, customer3, customer4, customer5 });
+            //Console.WriteLine(customer5.Acc);
+            
+
+            
+            while (true)
+            {
+                Console.WriteLine("Здравствуйте, Вас приветсвует БанК, Введите ваше имя..");
+                string Name = Console.ReadLine();
+                foreach (var person in customers)
+                    
+                {
+                    bool status = customers.Contains(customer1);
+                    if (person.Name == Name)
+                    {
+                        status = true;
+                        Console.WriteLine("Вы уже зарегистрированы");
+                    }
+                    else { Console.WriteLine("Вы не зарегисрированы, введите ваши данные..");
+
+                        int NewId = customers.Add(new Customer { Id = customers.IndexOf, }) ;
+                        string NewName = Console.ReadLine();
+
+                            Console.WriteLine("Введите ");
+                    }
+
+                }
+            }
+
+
+            //int Account = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
