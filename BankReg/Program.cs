90acdb33a76134931b8dd67e69b3295697afb3fc
Артур <arturskomarohov@gmail.com>
2021-07-31 00:08:17 +0300

    finish

--- BankReg/Program.cs
+++ BankReg/Program.cs
@@ -19,80 +19,77 @@ namespace BankReg
             customers.AddRange(new Customer[] { customer1, customer2, customer3, customer4, customer5 });
 
 
-
-            Console.WriteLine("Здравствуйте, Вас приветствует БанК, Введите ваше имя и фамилию..");
-            //while (Console.ReadLine() != "x")
-            // {
-
-
-
-            // string inputName = Console.ReadLine();
-            string NamePattern = "^[a-zA-Zа-яА-ЯёЁ'][a-zA-Zа-яА-Я-ёЁ' ]+[a-zA-Zа-яА-ЯёЁ']?$";
-            string AccPattern = "^[0-9]{5}$";
-            string inputName = Console.ReadLine();
-            while (!Regex.Match(inputName, NamePattern).Success)
+            Console.WriteLine("Здравствуйте, Вас приветствует БанК.. \n для продолжения нажмите Enter, для выхода x");
+            while (Console.ReadLine() != "x")
             {
-                if(!Regex.Match(inputName, NamePattern).Success) { 
-                    Console.WriteLine("Неверный формат ввода, проверьте на ошибки.");
+                Console.WriteLine("Введите ваше Имя и Фамилию..\n");
+
+
+                string NamePattern = "^[a-zA-Zа-яА-ЯёЁ'][a-zA-Zа-яА-Я-ёЁ' ]+[a-zA-Zа-яА-ЯёЁ']?$";
+                string AccPattern = "^[0-9]{5}$";
+
+                string inputName;
+                do
+                {
+                    inputName = Console.ReadLine();
+                    bool isNameValid = Regex.Match(inputName, NamePattern).Success;
+
+                    if (!isNameValid)
+                    {
+                        Console.WriteLine("Неверный формат ввода, проверьте на ошибки.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Теперь введите ваш номер счета в формате ХХХХХ\n");
+                    }
+                }
+                while (!Regex.Match(inputName, NamePattern).Success);
+
+
+                string inputAcc;
+                do
+                {
+                    inputAcc = Console.ReadLine();
+                    bool isAccValid = Regex.Match(inputAcc, AccPattern).Success;
+                    if (!isAccValid)
+                    {
+                        Console.WriteLine("Неверный формат ввода, проверьте на ошибки.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Данные проверены...\n");
+                    }
+                }
+                while (!Regex.Match(inputAcc, AccPattern).Success);
+                int inputAccInt = Convert.ToInt32(inputAcc);
+
+                Customer inputCustomer = new Customer { Name = inputName, Acc = inputAccInt };
+
+
+                //contains вызывает equals который мы уже переопределили
+                if (customers.Contains(inputCustomer))
+                {
+                    Console.WriteLine($"Здравствуйте {inputName},  Вы уже есть в базе \n");
                 }
                 else
                 {
-                    Console.WriteLine("Теперь введите ваш номер счета");
+                    Console.WriteLine("Вас нет в базе! Добавить Вас в клиенты? да / нет");
+                    if (Console.ReadLine() == "да")
+                    {
+                        customers.Add(inputCustomer);
+                        Console.WriteLine("Поздравляем, Вы добавлены в Клиенты \n");
+                    }
+                    else { Console.WriteLine("До свидания. \n"); }
+
                 }
-               
-                
+
+
+                foreach (Customer person1 in customers)
+                {
+                    Console.WriteLine((customers.IndexOf(person1) + 1) + $" {person1.Name} {person1.Acc}");
+                }
             }
-            
-            /*if (!Regex.Match(inputName, NamePattern).Success)
-            {
-                Console.WriteLine("Неверный формат ввода, проверьте на ошибки.");
-            }
-            else { Console.WriteLine("Теперь введите ваш номер счета"); }*/
-
-            Console.WriteLine("Теперь введите ваш номер счета");
-
-            int inputAcc = Convert.ToInt32(Console.ReadLine());
-            Customer inputCustomer = new Customer { Name = inputName, Acc = inputAcc };
-
-
-            //contains вызывает equals который мы уже переопределили
-
-            if (customers.Contains(new Customer { Name = inputName, Acc = inputAcc }))
-
-            {
-                Console.WriteLine("Вы " + inputName + "уже присутсвуете в базе");
-            }
-            else
-            {
-                Console.WriteLine("Вас нет в базе!");
-                customers.Add(inputCustomer);
-            }
-
-
-            foreach (Customer person1 in customers)
-            {
-                Console.WriteLine((customers.IndexOf(person1) + 1) + " " + person1.Name + person1.Acc);
-            }
-
-            // }
-
-
-
-
-
-
 
         }
-
-        public static Customer CustomerExists(Customer customer, Customer inputCustomer)
-        {
-            if (customer.Equals(inputCustomer))
-            { return null; }
-            else { return customer; }
-
-        }
-
-
-
     }
 }
